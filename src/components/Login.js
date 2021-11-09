287bc5c9674293deb2b38914741a998fb3948279
Matt Holmberg <mholmberg13@gmail.com>
2021-11-09 16:50:56 -0700

    Login submit added.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,51 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios';
 
-class Login extends React.Component {
+const initialState = {
+    email: '',
+    password: '',
+    err: '',
+    success: ''
+}
 
-    state = {
-        email: '',
-        password: '',
-        err: '',
-        success: ''
+function Login() {
+
+    const [user, setUser] = useState(initialState)
+    const {email, password, err, success} = user
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setUser({...user, [name]:value, err: '', success: ''})
     }
 
-    handleChange = (event) => {
-        this.setState({ [event.target.id]: event.target.value})
-    }
-
-    user = this.state
-    setUser = this.state
-
-    email = this.user.email
-    password = this.user.password
-    err = this.user.err
-    success = this.user.success
-
-    handleSubmit = async (e) => {
+    const handleSubmit = async e => {
         e.preventDefault()
         try {
-            const res = await axios.post('/user/login', {})
+            const res = await axios.post('/user/login', {email, password})
             console.log(res)
         } catch (err) {
-            err.response.data.msg && this.setUser({...this.user, err: err.response.data.msg, success: ''})
+            err.response.data.msg && 
+            setUser({...user, err: err.response.data.msg, success: ''})
         }
     }
-
-    render() {
         return (
             <div className="login-container">
                 <h2>Login</h2>
 
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="email">Email Address</label>
-                        <input type="text" placeholder="Enter Email" id="email" onChange={this.handleChange} value={this.state.email} name="email"/>
+                        <input type="text" placeholder="Enter Email" id="email" onChange={handleChange} value={email} name="email"/>
                     </div>
                     <div>
                         <label htmlFor="password">Password</label>
-                        <input type="text" placeholder="Enter Password" id="password" onChange={this.handleChange} value={this.state.password} name="password"/>
+                        <input type="text" placeholder="Enter Password" id="password" onChange={handleChange} value={password} name="password"/>
                     </div>
                     <div className="buttons">
                         <button type="submit">Login</button>
@@ -54,7 +49,6 @@ class Login extends React.Component {
                 </form>
             </div>
         )
-    }
 }
 
 export default Login;
